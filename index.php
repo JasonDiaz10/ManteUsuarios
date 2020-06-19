34a9f2fc83e2a512576a9b4e6d39fc18580f3b56
Jason Diaz <jdiaz@rodelu.cr>
2020-06-18 20:55:02 -0600

    v2-Reestructurar Proyecto

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<title>Mantenimiento de Usuarios</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/estilo.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <?php
+     require_once 'includes/contenido.php';
+     require_once 'includes/modalEditar.php';
+     require_once 'includes/modalAgregar.php';
+    ?>
+
+</body>
 </html>
